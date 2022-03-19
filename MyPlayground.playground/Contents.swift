4e6443cd2455db8ed48ad32836c9fc36688bd492
fatamah <fatiabbasali2018@gmail.com>
2022-03-19 10:40:26 +0300

    fati66

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,10 @@
 import UIKit
 
-var greeting = "Hello, playground"
+struct student{
+ var nema: String
+    var age : Int
+    var grade : Double
+    var atendence : Bool
+
+}
+var zahra: student = student(nema: "zahra", age:15 , grade: 98.85, atendence:true)
